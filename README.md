319239521d4cc16ba0f472f87f9911c4a78997d4
Saheb Giri <iamsahebgiri@gmail.com>
2022-07-23 01:01:17 +0530

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Fraud Auth App
---
+++ src/pages/index.tsx
@@ -0,0 +1,25 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import { trpc } from "../utils/trpc";
+
+const Home: NextPage = () => {
+  const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
+
+  return (
+    <>
+      <Head>
+        <title>Fraud Auth App</title>
+        <meta name="description" content="Create apps for using our api" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="container mx-auto flex flex-col items-center justify-center h-screen p-4">
+        <div className="pt-6 text-2xl text-blue-500 flex justify-center items-center w-full">
+          {hello.data ? <p>{hello.data.greeting}</p> : <p>Loading..</p>}
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Home;
